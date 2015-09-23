d507015561347abaefdba080b342ae6013d74a26
Rasberry <rasberry@hotmail.com>
2015-09-22 21:55:16 -0600

    =hooked up to 'express' using 'feed' to generate rss xml

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
-var googleAuth = require('google-auth-library');
+var GoogleAuth = require('google-auth-library');
 var async = require('async');
+var express = require('express');
+var Feed = require('feed');
 
 var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 var TOKEN_PATH = 'credentials/gmail-api-quickstart.json';
 
-
-
+/**
+ * Load client secrets from a local file.
+ * 
+ * @param {function} callback The callback which gets passed the json object from the file
+ */
 function getSecretFile(callback) {
-	// Load client secrets from a local file.
 	fs.readFile('client_secret.json', function processClientSecrets(err, content) {
 		if (err) {
 			console.log('Error loading client secret file: ' + err);
@@ -18,10 +22,6 @@ function getSecretFile(callback) {
 		} else {
 			callback(null,JSON.parse(content) || {})
 		}
-		// Authorize a client with the loaded credentials, then call the
-		// Gmail API.
-		//authorize(JSON.parse(content), listLabels);
-		//authorize(JSON.parse(content),listMessages);
 	});
 }
 
@@ -36,7 +36,7 @@ function authorize(credentials, callback) {
 	var clientSecret = credentials.installed.client_secret;
 	var clientId = credentials.installed.client_id;
 	var redirectUrl = credentials.installed.redirect_uris[0];
-	var auth = new googleAuth();
+	var auth = new GoogleAuth();
 	var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
 	// Check if we have previously stored a token.
@@ -98,29 +98,35 @@ function storeToken(token) {
 *
 * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
 */
-function listLabels(auth) {
-	var gmail = google.gmail('v1');
-	gmail.users.labels.list({
-		auth: auth,
-		userId: 'me',
-	}, function(err, response) {
-		if (err) {
-			console.log('The API returned an error: ' + err);
-			return;
-		}
-		var labels = response.labels;
-		if (labels.length == 0) {
-			console.log('No labels found.');
-		} else {
-			console.log('Labels:');
-			for (var i = 0; i < labels.length; i++) {
-				var label = labels[i];
-				console.log('- %s', label.name);
-			}
-		}
-	});
-}
+// function listLabels(auth) {
+// 	var gmail = google.gmail('v1');
+// 	gmail.users.labels.list({
+// 		auth: auth,
+// 		userId: 'me',
+// 	}, function(err, response) {
+// 		if (err) {
+// 			console.log('The API returned an error: ' + err);
+// 			return;
+// 		}
+// 		var labels = response.labels;
+// 		if (labels.length == 0) {
+// 			console.log('No labels found.');
+// 		} else {
+// 			console.log('Labels:');
+// 			for (var i = 0; i < labels.length; i++) {
+// 				var label = labels[i];
+// 				console.log('- %s', label.name);
+// 			}
+// 		}
+// 	});
+// }
 
+/**
+ * Gets the list of message id's
+ * 
+ * @param {google.auth.OAuth2} auth The OAuth2 client to use for authentication
+ * @param {getEventsCallback} callback The callback to call with the results
+ */
 function getMessageList(auth,callback) {
 	var gmail = google.gmail('v1');
 	gmail.users.messages.list({
@@ -243,17 +249,131 @@ function printMessageList(list,callback) {
 // 	getPageOfMessages(initialRequest, []);
 // }
 
-function main() {
-	async.waterfall([
-		getSecretFile
-		,authorize
-		,getMessageList
-		,getMessageListContent
-		,printMessageList
-	],function(err) {
-		if (err) {
-			console.log(err);
+var pushToFeed = function(list,rss,callback) {
+	list.sort(function(a,b) {
+		return b.internalDate - a.internalDate;
+	});
+	
+	for(var len=list.length, m=0; m<len; m++) {
+		var c = list[m];
+		var id = c.id;
+		var idate = c.internalDate;
+		var desc = c.snippet;
+		var heads = c.payload.headers;
+		var efrom = null;
+		var subj = null;
+		var edate = null;
+		var jsDate = new Date(parseInt(idate));
+		
+		for(var hl=heads.length, h=0; h<hl; h++) {
+			var name = heads[h].name;
+			if (name == 'From') {
+				efrom = heads[h].value;
+			} else if (name == 'Subject') {
+				subj = heads[h].value;
+			} else if (name == 'Date') {
+				edate = heads[h].value;
+			}
 		}
+	
+// title:          options.title,
+// link:           options.link,
+// description:    options.description,
+// date:           options.date,
+// image:          options.image,
+// author:         options.author,
+// contributor:    options.contributor,	// Atom only
+// guid:           options.guid,
+// id:             options.id,
+// content:        options.content,
+// copyright:		options.copyright	// Atom only				
+	
+		
+		rss.addItem({
+			title: subj
+			//,author: efrom
+			,id: id
+			,link: 'http://localhost/?'+id
+			,description: desc
+			,date: jsDate
+		});
+	}
+	callback();
+}
+
+function main() {
+	console.log("env = "+process.env.NODE_ENV);
+	var app = express();
+	
+	app.get('/rssmail',function(req,res) {
+		var rss = new Feed({
+			title:          'Rasberry Mail',
+			description:    'Rasberry Mail - rasberryred@gmail.com',
+			link:           'https://rasberry.us.to/'
+			,author: {
+				name:       'Rasberry',
+				email:      'rasberryred@gmail.com',
+				link:       'https://rasberry.us.to/'
+			}
+
+		});
+
+		async.waterfall([
+			getSecretFile
+			,authorize
+			,getMessageList
+			,getMessageListContent
+			,function(list,cb) { pushToFeed(list,rss,cb); }
+		],function(err) {
+			if (err) {
+				res.send(err,500);
+				//res.send('Not found', 404);
+			} else {
+				res.set('Content-Type', 'text/xml');
+				res.send(rss.render('rss-2.0'));
+				//res.set('Content-Type', 'application/atom+xml');
+				//res.send(rss.render('atom-1.0'));
+			}
+		});
+	});
+	
+	var server = app.listen(3000, function() {
+		var host = server.address().address;
+		var port = server.address().port;
+	
+		console.log('Example app listening at http://%s:%s', host, port);
 	});
 }
 main();
+
+// app.get('/rss', function(req, res) {
+
+// 	// Initializing feed object
+// 	var rss = new feed({
+// 		title:          'Rasberry Mail',
+// 		description:    'Rasberry Mail - rasberryred@gmail.com',
+// 		link:           'https://rasberry.us.to/'
+// 	});
+
+// 	// Function requesting the last 5 posts to a database. This is just an
+// 	// example, use the way you prefer to get your posts.
+// 	Post.findPosts(function(posts, err) {
+// 		if(err)
+// 			res.send('404 Not found', 404);
+// 		else {
+// 			for(var key in posts) {
+// 				feed.item({
+// 					title:          posts[key].title,
+// 					link:           posts[key].url,
+// 					description:    posts[key].description,
+// 					date:           posts[key].date
+// 				});
+// 			}
+// 			// Setting the appropriate Content-Type
+// 			res.set('Content-Type', 'text/xml');
+
+// 			// Sending the feed as a response
+// 			res.send(feed.render('rss-2.0'));
+// 		}
+// 	});
+// });
